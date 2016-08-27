8f12fb6f67d411800de582cf53fb8af78468fd42
sedayoldas <sedaylds@windowslive.com>
2016-08-28 00:22:15 +0300

    Commit

---
+++ CountDown/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CountDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtHour
+            // 
+            this.txtHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHour.Location = new System.Drawing.Point(55, 49);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(58, 35);
+            this.txtHour.TabIndex = 0;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMin.Location = new System.Drawing.Point(146, 49);
+            this.txtMin.MaxLength = 2;
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(58, 35);
+            this.txtMin.TabIndex = 1;
+            // 
+            // txtSec
+            // 
+            this.txtSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSec.Location = new System.Drawing.Point(240, 49);
+            this.txtSec.MaxLength = 2;
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(58, 35);
+            this.txtSec.TabIndex = 2;
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHour.Location = new System.Drawing.Point(78, 114);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(26, 29);
+            this.lblHour.TabIndex = 3;
+            this.lblHour.Text = "0";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMin.Location = new System.Drawing.Point(166, 114);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(26, 29);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "0";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSec.Location = new System.Drawing.Point(257, 114);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(26, 29);
+            this.lblSec.TabIndex = 5;
+            this.lblSec.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(122, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(216, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ":";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(134, 218);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 32);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(55, 8);
+            this.maskedTextBox1.Mask = "00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.PromptChar = ' ';
+            this.maskedTextBox1.Size = new System.Drawing.Size(59, 35);
+            this.maskedTextBox1.TabIndex = 9;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(145, 8);
+            this.maskedTextBox2.Mask = "00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.PromptChar = ' ';
+            this.maskedTextBox2.Size = new System.Drawing.Size(59, 35);
+            this.maskedTextBox2.TabIndex = 10;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox3.Location = new System.Drawing.Point(239, 8);
+            this.maskedTextBox3.Mask = "00";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.PromptChar = ' ';
+            this.maskedTextBox3.Size = new System.Drawing.Size(59, 35);
+            this.maskedTextBox3.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 328);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtHour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CountDown/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hour, minute, second;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int.TryParse(txtHour.Text, out hour);
+            int.TryParse(txtMin.Text, out minute);
+            int.TryParse(txtSec.Text, out second);
+
+            //değerlerden negatif olan varsa pozitife çevir
+            if (second < 0 || minute<0 || hour<0)
+            {
+                second = (int)Math.Abs(second);
+                minute = (int)Math.Abs(minute);
+                hour = (int)Math.Abs(hour);
+            }
+
+            //dakika ve saniye eğer 60dan fazla ise dakika veya saat arttırılsın
+            if (second > 60)
+            {
+                int plus = second / 60;
+                minute += plus;
+                second %= 60;
+            }
+            if (minute > 60)
+            {
+                int plus = minute / 60;
+                hour += plus;
+                minute %= 60;
+            }
+            //değerler asıl durumlarına dönsün
+            txtHour.Text = hour.ToString();
+            txtMin.Text = minute.ToString();
+            txtSec.Text = second.ToString();
+            //txt leri kullanıma kapatalım
+            txtHour.Enabled=false;
+            txtMin.Enabled=false;
+            txtSec.Enabled = false;
+
+            //gecikme olmaması için değerler hemen gösterilsin
+            lblHour.Text = hour.ToString();
+            lblMin.Text = minute.ToString();
+            lblSec.Text = second.ToString();
+
+            timer1.Start();
+            btnStart.Enabled = false;//eski değerlerle yeniden geri sayımı başlatmasın
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+          
+            if (second==0)
+            {
+                if (minute > 0)
+                {
+                    minute--;
+                    second = 59;
+                }
+                else
+                {
+                    if (hour > 0)
+                    {
+                        hour--;
+                        minute = 59;
+                        second = 59;
+                    }
+                }
+            }
+            second--;
+
+
+            lblHour.Text = hour.ToString();
+            lblMin.Text = minute.ToString();
+            lblSec.Text = second.ToString();
+
+            if (hour == 0 && minute == 0 && second == 0)
+            {
+                timer1.Stop();
+                //result kullanıcının yes veya no butonunun değerini alır
+                DialogResult result= MessageBox.Show("Do you want to start a new countdown?", "Time Is Out!..",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+                if (result==DialogResult.Yes)
+                {//kullanıcı yeniden geri sayım yapılmasını isterse
+                    btnStart.Enabled = true;
+                    txtHour.Enabled = true;
+                    txtMin.Enabled = true;
+                    txtSec.Enabled = true;
+                    txtHour.Clear();
+                    txtMin.Clear();
+                    txtSec.Clear();
+                }
+                else
+                {//hayır derse uygulamayı sonlandır
+                    Thread.Sleep(3000);
+                    Application.Exit();
+                }
+            }
+           
+
+        }
+    }
+}
